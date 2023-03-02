af986f7f2a5751266139108da37b44f4716048d4
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-03-03 00:00:20 +0100

    add inter font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex w-screen h-screen items-center justify-center">
 	<div>
-		<h1 class="text-3xl">Viktor Andersson</h1>
+		<h1 class="text-3xl font-inter">Viktor Andersson</h1>
 	</div>
 </div>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultConfig = require('tailwindcss/defaultConfig');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		fontFamily: {
+			inter: ['Inter', ...defaultConfig.theme.fontFamily.sans]
+		}
 	},
 	plugins: []
 };
