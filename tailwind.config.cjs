be82e2beb54f6fe50f35f7912656757c49ac98e4
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-03-04 21:28:20 +0100

    add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,17 @@ const defaultConfig = require('tailwindcss/defaultConfig');
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			canary: '#fee900',
+			flame: '#ec4e20',
+			'forest-green': '#248232',
+			'oxford-blue': '#0a122a',
+			'celestial-blue': '#009ddc',
+			night: '#03120e',
+			'ghost-white': '#f7f7ff'
+		},
 		extend: {
 			animation: {
 				marquee: '10s linear 0s infinite normal running marquee'
