65fad303acb0eced58c7ee90b2cc9ed334b4e1f8
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-03-04 15:59:24 +0100

    fix animation

--- src/components/UnderConstruction.svelte
+++ src/components/UnderConstruction.svelte
@@ -1,21 +1,53 @@
-<div class="flex flex-row items-center overflow-x-hidden w-full bg-yellow-400 shadow">
-	<animated.ul class="inline-block whitespace-nowrap animate-marquee">
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
-		<animated.li class="inline-block relative">/// Under Construction</animated.li>
+<div class="flex flex-row overflow-x-hidden w-full bg-yellow-400 shadow">
+	<animated.ul class="flex min-w-full flex-none flex-row items-center whitespace-nowrap">
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
+		<animated.li class="inline-block relative animate-marquee">
+			&nbsp/// Under Construction</animated.li
+		>
 	</animated.ul>
 </div>
 
