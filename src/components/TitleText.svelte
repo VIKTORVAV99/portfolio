bff3d02217f18a7a560b4c86cc184885f1c49af5
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-03-05 23:47:36 +0100

    add professional icons

---
+++ src/components/TitleText.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let text: string;
+</script>
+
+<!-- @component
+TitleText component that auto scales the text size based on the screen size.
+-->
+
+<h1
+	class="
+    text-4xl
+    lg:text-5xl
+    xl:text-6xl font-inter"
+>
+	{text}
+</h1>
