66711c28ebcba0fd985ae80e5ca2cd4a782835d2
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-03-06 10:36:39 +0100

    prettier

--- src/components/IconSet.svelte
+++ src/components/IconSet.svelte
@@ -2,7 +2,7 @@
 	import Icon from 'svelte-awesome';
 	import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 	import { faSquareGithub } from '@fortawesome/free-brands-svg-icons/faSquareGithub';
-  export let scale: number = 3;
+	export let scale: number = 3;
 </script>
 
 <!--
@@ -12,9 +12,9 @@
   -->
 <div class="flex flex-row justify-center items-center mt-2">
 	<a title="LinkedIn" href="https://www.linkedin.com/in/viktor-va-andersson/" class="m-3">
-		<Icon data={faLinkedin} scale={scale} class="text-dark-gray hover:text-linkedin-blue" />
+		<Icon data={faLinkedin} {scale} class="text-dark-gray hover:text-linkedin-blue" />
 	</a>
 	<a title="GitHub" href="http://github.com/VIKTORVAV99" class="m-3">
-		<Icon data={faSquareGithub} scale={scale} class="text-dark-gray hover:text-white" />
+		<Icon data={faSquareGithub} {scale} class="text-dark-gray hover:text-white" />
 	</a>
 </div>
