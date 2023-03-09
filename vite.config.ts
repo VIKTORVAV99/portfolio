540f6a3ea405c148073320ff0648accd76648c3a
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-03-09 11:40:48 +0100

    go back to just build path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 		sveltekit(),
 		Sitemap({
 			hostname: 'https://viktor.andersson.tech',
-			outDir: process.env.CI ? './home/runner/work/personal-website/build' : 'build',
+			outDir: 'build',
 			robots: [{ userAgent: '*', allow: '/', disallow: '' }],
 			exclude: ['/404']
 		})
