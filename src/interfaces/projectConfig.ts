dce3c5b7bda2c69acbf0a82b0dce6ddc21d4dc59
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-04-06 09:37:23 +0200

    prettier

--- src/interfaces/projectConfig.ts
+++ src/interfaces/projectConfig.ts
@@ -2,20 +2,20 @@ import type { Url } from 'url';
 
 export interface ProjectConfig {
 	title: string;
-  dateStarted: string;
-  dateCompleted: string | "ongoing";
+	dateStarted: string;
+	dateCompleted: string | 'ongoing';
 	showcase: boolean;
 	type: 'personal' | 'professional' | 'education';
 	links: Array<{ title: string; url: Url }>;
 	keywords: Array<string>;
 	technologies: Array<{ name: string; url: Url; description: string; preview: boolean }>;
-  preview: {
-    description: string;
-    image?: string
-  };
-  details: {
-    tagline: string;
-    description: string;
-    images?: Array<string>;
-  }
+	preview: {
+		description: string;
+		image?: string;
+	};
+	details: {
+		tagline: string;
+		description: string;
+		images?: Array<string>;
+	};
 }
