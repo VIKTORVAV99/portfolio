0d445c5ad5ae019f683fef9f1ff4f072dd4f3ea2
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-05-18 18:07:05 +0200

    prettier

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,11 +1,10 @@
-import projects from "$data/projects.json"
+import projects from '$data/projects.json';
 
-const DOMAIN = "https://viktor.andersson.tech/"
-
+const DOMAIN = 'https://viktor.andersson.tech/';
 
 export async function GET() {
-    return new Response(
-			`
+	return new Response(
+		`
         <?xml version="1.0" encoding="UTF-8" ?>
         <urlset
             xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
@@ -18,12 +17,12 @@ export async function GET() {
           </url>
           ${projects.map((projects) => `<url><loc>${DOMAIN + 'projects/' + projects.id}</loc></url>`)}
         </urlset>`
-				.replaceAll(',', '')
-				.trim(),
-			{
-				headers: {
-					'Content-Type': 'application/xml'
-				}
+			.replaceAll(',', '')
+			.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
 			}
-		);
+		}
+	);
 }
