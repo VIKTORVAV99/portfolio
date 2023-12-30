7d19f2542705835a1d805c3dc1911b88cc3b4b8b
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2023-12-30 20:46:38 +0100

    make group by faster

--- src/helpers/groupBy.ts
+++ src/helpers/groupBy.ts
@@ -1,10 +1,12 @@
-export const groupBy = <Entry, Key extends keyof never>(list: Entry[], getKey: (item: Entry) => Key) =>
-	list.reduce(
-		(previous, currentItem) => {
-			const group = getKey(currentItem);
-			if (!previous[group]) previous[group] = [];
-			previous[group].push(currentItem);
-			return previous;
-		},
-		{} as Record<Key, Entry[]>
-	);
+export const groupBy = <Entry, Key extends keyof never>(list: Entry[], getKey: (item: Entry) => Key) => {
+	const groups = new Map<Key, Entry[]>();
+
+	for (const item of list) {
+		const group = getKey(item);
+		const groupItems = groups.get(group) || [];
+		groupItems.push(item);
+		groups.set(group, groupItems);
+	}
+
+	return Object.fromEntries(groups);
+};
