64a3dacdc991d23247c836603cec754234dfdcf6
Viktor Andersson <30777521+VIKTORVAV99@users.noreply.github.com>
2024-02-05 00:55:23 +0100

    temp_fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,7 @@ import type { ProjectConfig } from '$interfaces/projectConfig';
 
 export const load = ( ({fetch}) => {
 	const json = projects.filter((entry) => entry.showcase) as ProjectConfig[];
-	const personal_info = fetch('/api/personal_data/').then((r) => r.json());
+	const personal_info = fetch('/api/personal_data/');
 	return {
 		projects: json,
 		personalData: personal_info
