8a793754f7d824fee20c45484bc646981aed323e
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2026-03-11 22:30:17 +0100

    clean up footer

--- apps/frontend/src/components/Footer.svelte
+++ apps/frontend/src/components/Footer.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
+	const year = $derived(new Date().getFullYear());
 </script>
 
-<div class="flex flex-col justify-center preset-tonal-primary font-light">
-	<div class="flex flex-col w-screen justify-center items-center">
-		<small>{new Date().getFullYear()} &copy; Viktor Andersson </small>
-		<small
-			>Source code licensed under <a
-				class="text-secondary-600 dark:text-secondary-300"
-				href="https://github.com/VIKTORVAV99/personal-website/blob/main/LICENSE"
-				rel="license">MIT license</a
-			></small
-		>
-	</div>
+<div class="flex flex-col w-screen justify-center items-center preset-tonal-primary font-light">
+	<small>{year} &copy; Viktor Andersson </small>
+	<small
+		>Source code licensed under <a
+			class="text-secondary-600 dark:text-secondary-300"
+			href="https://github.com/VIKTORVAV99/personal-website/blob/main/LICENSE"
+			rel="license">MIT license</a
+		></small
+	>
 </div>
