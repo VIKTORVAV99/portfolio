69386444747608e0b1c7067b45499650a82088f3
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2026-03-23 23:55:57 +0100

    format

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { imagetools } from "vite-imagetools";
 import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
 
 export default defineConfig({
   plugins: [
