d267a16ca7ad4d6e6d9830adaf8f2cfed1a194cf
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2026-03-30 21:13:53 +0200

    fix race condition in service worker

--- apps/frontend/src/service-worker.ts
+++ apps/frontend/src/service-worker.ts
@@ -97,7 +97,6 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   const url = new URL(event.request.url);
-  if (url.protocol === "chrome-extension:") return;
   if (url.origin !== self.location.origin) return;
 
   const respond = async () => {
